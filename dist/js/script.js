5e1161989057b2540fb2055c2a42cfbb5d7dbf2b
Charlieriveraa <charlierivera541@gmail.com>
2024-03-25 15:20:58 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,40 @@
+const burgir = document.querySelector(".burgir__menu");
+const headerA = document.querySelector(".header__nav");
+burgir.addEventListener("click", () => {
+    burgir.classList.toggle("active");
+    headerA.classList.toggle("active");
+});
+
+// const faqinfo = document.querySelectorAll(".faq__infos");
+// faqinfo.forEach((infos) => {
+//     infos.addEventListener("click", ()=>{
+//         infos.classList.toggle("active");
+//     });
+// });
+
+
+// const faqsLinks = document.querySelectorAll(".faq__links");
+// const faqsContent = document.querySelectorAll(".faq__content");
+// faqsLinks.forEach((faq) => {
+//   faq.addEventListener("click", () => {
+//     removeActiveFaqs();
+//     faq.classList.add("active");
+//     const activeContent = document.querySelector(`#${faq.id}-content`);
+//     removeActiveContent();
+//     activeContent.classList.add("active");
+//   });
+// });
+
+// function removeActiveContent() {
+//   faqsLinks.forEach((content) => {
+//     content.classList.remove("active");
+//   });
+// };
+
+// function removeActiveContent() {
+//   faqsContent.forEach((content) => {
+//     content.classList.remove("active");
+//   });
+// };
+
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+       FBSBanner: "url(/dist/img/fbs-banner-bg.png)",
+        offerbg: "url(/dist/img/whatoffer.jpg)",
+        bgsvg: "url(/dist/img/bg3.svg)",
+      },
+      colors:{
+        dark:"#2b2b2b",
+        light:"#ffffff",
+        redz: "#e64737",
+        lightredz: "#ff4041",
+        fbscolor: "#6a103f",
+      }
+    },
+  },
+  plugins: [],
+}
